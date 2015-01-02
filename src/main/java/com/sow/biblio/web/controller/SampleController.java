db59910c3cd47fba899e239f28e0abd851fbbbe1
Sowjanya <sowjanya@Sowjanyas-MacBook-Pro.local>
2015-01-02 16:45:47 -0500

    Added message resources

--- src/main/java/com/sow/biblio/web/controller/SampleController.java
+++ src/main/java/com/sow/biblio/web/controller/SampleController.java
@@ -1,6 +1,9 @@
 package com.sow.biblio.web.controller;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.MessageSource;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +16,15 @@ public class SampleController {
 	@Value("${testProp1}")
 	private String testProp1;
 	
+	@Resource
+	private MessageSource messageSource;
+	
 	@RequestMapping("/sample.htm")
 	public String printWelcome(ModelMap model) {
+				
+		String testMsg = messageSource.getMessage("testMsg", null, null);
 		
-		System.out.println("\n*****in printWelcome testProp1:" + testProp1);
+		System.out.println("\n*****in printWelcome testProp1, testMsg:" + testProp1 + ", " + testMsg);
 		
 		return "sample";
 	}
--- src/main/webapp/WEB-INF/jsp/sample.jsp
+++ src/main/webapp/WEB-INF/jsp/sample.jsp
@@ -1,5 +1,9 @@
 <html>
 <body>
 <h2>Sample.jsp</h2>
+
+<h5><spring:message code="testMsg" /></h5>
+
+
 </body>
 </html>
